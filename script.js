b1cf94a3e77e3efaee3dfe4ff004d5b0e5e559ca
Salih Can ÖZEL <can.ozel@bil.omu.edu.tr>
2015-04-15 18:49:43 +0300

    angular %80 tamamlandı.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Code goes here
+
+// Code goes here
 angular.module('sinav', ['ui.bootstrap']);
 angular.module('sinav').controller('sinavapp', function($scope, $http) {
 
@@ -22,9 +24,41 @@ angular.module('sinav').controller('sinavapp', function($scope, $http) {
     }
   }
   
-  $scope.addClass = function(checked) {
-    if (checked){
-      return "list-group-item-success";
-    }
+  $scope.answers = [];
+  $scope.selected = function(ans,currentTab){
+    $scope.answers[currentTab] = ans-1;
   }
+  
+  $scope.answered = function(){
+    var count = 0;
+    for (i = 0; i < $scope.answers.length; i++) { 
+      if ($scope.answers[i] !== null){
+        count += 1;
+      }
+    }
+    return count; 
+  }
+  
+  $scope.again = function(){
+    $scope.again = false 
+  }
+  
+  $scope.end = function(){
+    ModalService.showModal({
+            templateUrl: 'modal.html',
+            controller: "ModalController"
+        }).then(function(modal) {
+            modal.element.modal();
+            modal.close.then(function(result) {
+                $scope.message = "You said " + result;
+            });
+        });
+  }
+  
+  angular.module('sinav').controller('ModalController', function($scope, close) {
+    
+   $scope.close = function(result) {
+    close(result, 500); // close, but give 500ms for bootstrap to animate
+   };
+  });
 });
